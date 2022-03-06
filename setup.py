dcb1064b719f5cfc9339363722763efd515a4829
Kur0r0 <limaalyson@hotmail.com>
2022-03-05 22:15:02 -0300

    tornando instalável

---
+++ setup.py
@@ -0,0 +1,13 @@
+from gettext import install
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name = "overloading",
+    version = "1.0",
+    url = "https://github.com/toinnn/Python_Overloading",
+    packages = find_packages() ,
+    license = "LICENSE",
+    install_requires = ["certifi==2021.10.8" , "wincertstore"]
+)
