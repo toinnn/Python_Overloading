d50d9fcedbf0765d44b68e9c5e73e42358f0cf2b
Kur0r0 <limaalyson@hotmail.com>
2022-03-05 22:27:45 -0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+from overloading import override , Overload
+from overloading.overloading import overload
+
+class yourClass(override):
+    def __init__(self , yourArgs ) -> None:
+        self.yourArgs = yourClass
+
+    @overload
+    def yourFunction(a :int ):
+        print(f"I am a int {a}")
+    
+    @overload
+    def yourFunction(a :int , b : float ):
+        print(f"I am a int {a} and a float {b}")
